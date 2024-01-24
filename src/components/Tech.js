32975986712cd4171458fbd7229d6e5f78040a12
Eva Li <evali3101@gmail.com>
2024-01-24 17:41:40 -0500

    edited and added data tech pg

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,35 +1,62 @@
 import React from "react";
-import { techDetails } from "./techDetails";
+import { techDetails, techDetail1 } from "./techDetails";
 import TechFxn from "./techFxn";
 
 export default function Tech() {
   return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <h1 className="w-[76%] text-center sm:text-start text-2xl sm:text-4xl mb-8 text-[#4d4a48] font-bold">
-        Technologies I have used...
-      </h1>
-      <div className="carousel-container">
-        <div className="carousel-track">
-          {Object.keys(techDetails).map((detailKey) => {
-            return (
-              <TechFxn
-                png={techDetails[detailKey].png}
-                title={techDetails[detailKey].title}
-                key={techDetails[detailKey].key}
-              ></TechFxn>
-            );
-          })}
-          {Object.keys(techDetails).map((detailKey) => {
-            return (
-              <TechFxn
-                png={techDetails[detailKey].png}
-                title={techDetails[detailKey].title}
-                key={techDetails[detailKey].key}
-              ></TechFxn>
-            );
-          })}
+    <div className="h-full flex flex-col justify-start items-center">
+      <section className="w-[76%] h-[25%] flex items-center text-center sm:text-start">
+        <h1 className="text-2xl sm:text-4xl text-[#4d4a48] font-bold">
+          Technologies I've used...
+        </h1>
+      </section>
+
+      <section className="h-[75%] flex flex-col justify-evenly">
+        <div className="carousel-container">
+          <div className="carousel-track">
+            {Object.keys(techDetails).map((detailKey) => {
+              return (
+                <TechFxn
+                  png={techDetails[detailKey].png}
+                  title={techDetails[detailKey].title}
+                  key={techDetails[detailKey].key}
+                ></TechFxn>
+              );
+            })}
+            {Object.keys(techDetails).map((detailKey) => {
+              return (
+                <TechFxn
+                  png={techDetails[detailKey].png}
+                  title={techDetails[detailKey].title}
+                  key={techDetails[detailKey].key}
+                ></TechFxn>
+              );
+            })}
+          </div>
         </div>
-      </div>
+        <div className="carousel-container">
+          <div className="carousel-track">
+            {Object.keys(techDetail1).map((detailKey) => {
+              return (
+                <TechFxn
+                  png={techDetail1[detailKey].png}
+                  title={techDetail1[detailKey].title}
+                  key={techDetail1[detailKey].key}
+                ></TechFxn>
+              );
+            })}
+            {Object.keys(techDetail1).map((detailKey) => {
+              return (
+                <TechFxn
+                  png={techDetail1[detailKey].png}
+                  title={techDetail1[detailKey].title}
+                  key={techDetail1[detailKey].key}
+                ></TechFxn>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/techDetails.js
+++ src/components/techDetails.js
@@ -8,46 +8,64 @@ import psql from "../png/psql.png";
 import react from "../png/react.png";
 import sql from "../png/sequelize.png";
 import tail from "../png/tailwind.svg";
+import vercel from "../png/vercel.png";
+import netlify from "../png/netlify.png";
+import boot from "../png/boot.png";
 
 export const techDetails = {
- 0: {
+  0: {
     png: css3,
-    title: "css3"
- },
- 1: {
+    title: "css3",
+  },
+  1: {
     png: firebase,
-    title: "firebase"
- },
- 2: {
+    title: "firebase",
+  },
+  2: {
     png: github,
-    title: "github"
- },
- 3: {
-    png: html, 
-    title: "html"
- },
- 4: {
+    title: "github",
+  },
+  3: {
+    png: html,
+    title: "html",
+  },
+  4: {
     png: js,
-    title: "js"
- },
- 5: {
+    title: "js",
+  },
+  5: {
     png: node,
-    title: "node"
- },
- 6: {
-    png: psql, 
-    title: "psql"
- },
- 7: {
-    png: react, 
-    title: "react"
- },
- 8: {
+    title: "node",
+  },
+  6: {
+    png: psql,
+    title: "psql",
+  }
+};
+
+export const techDetail1 = {
+  7: {
+    png: react,
+    title: "react",
+  },
+  8: {
     png: sql,
-    title: "sql"
- },
- 9: {
+    title: "sql",
+  },
+  9: {
     png: tail,
-    title: "tail"
- }
+    title: "tail",
+  },
+  10: {
+    png: boot,
+    title: "boot"
+  },
+  11: {
+    png: vercel,
+    title: "vervel"
+  },
+  12: {
+    png: netlify,
+    title: "netlify"
+  }
 };

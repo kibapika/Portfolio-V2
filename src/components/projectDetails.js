23a98c3f4ff08850a3995ad127ae70d304eab7b7
Eva Li <evali3101@gmail.com>
2024-05-16 14:13:06 -0400

    updated projectdetails png

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -1,12 +1,12 @@
-// import bx from "../img/bx.png";
-// import ecommerce from "../img/ecommercePic.png";
-// import eve from "../img/evePic.png";
-// import mindful from "../img/mindfulPic.png";
-// import temps from "../img/tempsPic.png";
+import bx from "../png/bx.png";
+import ecommerce from "../png/ecommercePic.png";
+import eve from "../png/evePic.png";
+import mindful from "../png/mindfulPic.png";
+import temps from "../png/tempsPic.png";
 
 export const projectDetails = [
   {
-    // png: bx,
+    png: bx,
     title: "BunnyXpress - Art Portfolio",
     desc: "My small business (BunnyXpress) art portfolio website built with React App and hosted by Netlify. Desktop and mobile friendly!",
     link: "https://bunnyxpress.netlify.app/",
@@ -14,7 +14,7 @@ export const projectDetails = [
     tech: ["React ", "Netlify ", "Tailwind ", "CSS "],
   },
   {
-    // png: mindful,
+    png: mindful,
     title: "Mindful - Mental Health Awareness ",
     desc: "Capstone Project. An interactive mental health website with core funcationality including a moodtracer, journal and more to boost and refine mindfulness.",
     link: "https://mindful-site.web.app/",
@@ -22,14 +22,14 @@ export const projectDetails = [
     tech: ["React ", "Node.js ", "Firebase ", "Tailwind ", "Firestore "],
   },
   {
-    // png: ecommerce,
+    png: ecommerce,
     title: "Soleful - Shoe E-commerce Site ",
     desc: "Recreation of a e-commerce site based on shoes. A CRUD website focusing on React and Javascript.",
     git: "https://github.com/kibapika/Soles",
     tech: ["React ", "PSQL ", "React-Redux ", "Axios ", "Express "],
   },
   {
-    // png: eve,
+    png: eve,
     title: "EVE - Search Engine ",
     desc: "Recreation of a search engine app using React.js and axios. Mobile friendly!",
     link: "https://eve-evali.vercel.app/",
@@ -37,7 +37,7 @@ export const projectDetails = [
     tech: ["React ", "Node.js ", "Bootstrap ", "Axios ", "CSS "],
   },
   {
-    // png: temps,
+    png: temps,
     title: "Temps - Weather App ",
     desc: "Recreation of a weather app using React.js and axios. Mobile friendly!",
     link: "https://github.com/kibapika/Temps",

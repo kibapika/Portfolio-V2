3345ef74421e9e3851d4c00413050e00d61e9320
Eva Li <evali3101@gmail.com>
2024-05-16 14:09:50 -0400

    updated gitingot

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import facePic from "../img/evali.jpeg";
+import facePic from "../img/evaLi.jpeg";
 
 const About = () => {
   useEffect(() => {
